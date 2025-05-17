92ff70d4445da9ec8b36c93e089389211f26bb46
Thiago Abreu <tabreu.br@gmail.com>
2025-05-16 21:13:21 -0300

    User insertion with POST

--- src/main/java/com/thiagoabreu/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagoabreu/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.thiagoabreu.workshopmongo.services;
 
 import com.thiagoabreu.workshopmongo.domain.User;
+import com.thiagoabreu.workshopmongo.dto.UserDTO;
 import com.thiagoabreu.workshopmongo.repository.UserRepository;
 import com.thiagoabreu.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,11 @@ public class UserService {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
