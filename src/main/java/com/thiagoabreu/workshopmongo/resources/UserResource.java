705fc467d348c4a89b30680c37c8b10ec3301a91
Thiago Abreu <tabreu.br@gmail.com>
2025-06-24 15:35:22 -0300

    Endpoint to return a user's posts

--- src/main/java/com/thiagoabreu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thiagoabreu/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.thiagoabreu.workshopmongo.resources;
 
+import com.thiagoabreu.workshopmongo.domain.Post;
 import com.thiagoabreu.workshopmongo.domain.User;
 import com.thiagoabreu.workshopmongo.dto.UserDTO;
 import com.thiagoabreu.workshopmongo.services.UserService;
@@ -54,4 +55,12 @@ public class UserResource {
         service.update(obj);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok(user.getPosts());
+
+    }
+
 }
